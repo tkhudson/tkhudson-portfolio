93641b07ab4e579648187befbdebefad8c0c82ca
Tyler Hudson <tylerkhudson@gmail.com>
2024-09-26 11:34:47 -0500

    added menu navigation, basic

---
+++ src/components/menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Menu() {
+    return (
+        <nav className="menu">
+            <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/aboutme">About Me</a></li>
+            <li><a href="/resume">Resume</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Menu;
