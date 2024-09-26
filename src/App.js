bcd388ca33b3f01f89b4c2fa11a51bf2fdb8509f
tkhudson <tylerkhudson@gmail.com>
2024-09-26 06:36:01 -0500

    upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import styles from './App.css';
+
+function App() {
+  return (
+    <div>
+      <styles
+      <Header />
+      <p>Welcome to DevByCloud!</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <header>
+            <h1>Thanks for visiting!</h1>
+        </header>
+    );
+}
+
+export default Footer;
